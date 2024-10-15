cd9fee62c3998bd97dfeb974b2ca7bebff706679
Shashank Hegde <86239282+ShashankHegde8@users.noreply.github.com>
2024-10-15 16:47:54 +0530

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,54 @@
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        LinkedList l1 = new LinkedList();
+        l1.display();
+        l1.addFirst(1);
+        l1.addFirst(2);
+        l1.display();
+        l1.addLast(3);
+        l1.addLast(4);
+        l1.display();
+    }
+}
